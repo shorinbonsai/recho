c445041c1d4faaed0fbd0479a5c83b5928650cba
James Sargant <shorinbonsai@gmail.com>
2022-07-11 12:24:53 -0400

    changed clap to V3

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,30 @@
-use clap::{App, Arg};
+use clap::{Arg, Command};
 
 fn main() {
-    let matches = App::new("recho")
+    let matches = Command::new("recho")
         .version("0.1.0")
         .author("James Sargant <shorinbonsai@gmail.com>")
         .about("Rust implementation of Echo")
         .arg(
-            Arg::with_name("text")
+            Arg::new("text")
                 .value_name("TEXT")
                 .help("Input text")
                 .required(true)
                 .min_values(1),
         )
         .arg(
-            Arg::with_name("omit_newline")
-                .short("n")
+            Arg::new("omit_newline")
+                .short('n')
                 .help("Do not print newline")
                 .takes_value(false),
         )
         .get_matches();
 
-    let text = matches.values_of_lossy("text").unwrap();
+    let text: Vec<String> = matches
+        .get_many("text")
+        .expect("`text`is required")
+        .cloned()
+        .collect();
     let omit_newline = matches.is_present("omit_newline");
     print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
 }
